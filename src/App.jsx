8ea738e32f944be382046f407fa042baa5e4ca13
Al Mahmud <alma.mahmud.dev@gmail.com>
2026-03-02 14:07:25 +0600

    Import and use Faq component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navber from './components/Navber'
 import FeaturedCourse from './components/FeaturedCourse'
-
+import Faq from './components/Faq'
 const App = () => {
   return (
     <>
     <Navber></Navber>
     <FeaturedCourse></FeaturedCourse>
+      <Faq></Faq>
     </>
   )
 }

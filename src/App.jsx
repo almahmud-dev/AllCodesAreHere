991ecdbc0abc8a19c5293c110c59a5eccdc57e37
Al Mahmud <alma.mahmud.dev@gmail.com>
2026-03-09 19:24:53 +0600

    Add ImageResizer component to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navber from './components/Navber'
 import FeaturedCourse from './components/FeaturedCourse'
 import Faq from './components/Faq'
+import Faq from './components/ImageResizer'
 const App = () => {
   return (
     <>
     <Navber></Navber>
     <FeaturedCourse></FeaturedCourse>
       <Faq></Faq>
+      <ImageResizer/>
     </>
   )
 }
